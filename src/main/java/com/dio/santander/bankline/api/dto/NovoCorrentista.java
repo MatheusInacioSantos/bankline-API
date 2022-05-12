edf025fe368779c5fabf9e2b9ff8cdd8d3161a0c
MatheusInacioSantos <72869638+MatheusInacioSantos@users.noreply.github.com>
2022-05-12 17:31:19 -0300

    final version project

---
+++ src/main/java/com/dio/santander/bankline/api/dto/NovoCorrentista.java
@@ -0,0 +1,22 @@
+package com.dio.santander.bankline.api.dto;
+
+public class NovoCorrentista {
+	private String name;
+	private String cpf;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+
+}
--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -29,6 +29,9 @@ public class Movimentacao {
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
 	
+	@Column(name = "id_conta")
+	private Integer idConta;
+	
 	// gets and sets
 	public Integer getId() {
 		return id;
@@ -60,5 +63,10 @@ public class Movimentacao {
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
 	}
-
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
 }
