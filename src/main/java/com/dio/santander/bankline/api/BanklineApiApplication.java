3d274eb795296d9261a4bb8736cad245072a845e
MatheusInacioSantos <72869638+MatheusInacioSantos@users.noreply.github.com>
2022-05-15 17:56:39 -0300

    alterações de no set/get do correntista

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -8,7 +8,7 @@ public class BanklineApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
-		// endereço na nuvem
+		// localhost 8090
 	}
 
 }
